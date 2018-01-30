fd2c4b541c04f7c2c9ef680938e0cb5e8d282301
LissetteMarie22 <ramoslissette22@yahoo.com>
2018-01-30 13:10:26 -0500

    This program adds all of the numbers the user submits up and presents the total at the end.

---
+++ SumOfAllNumbs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter one number at a time, and I will add them together.");
+            Console.WriteLine("Please type in your first number.");
+            int numbHolder = 0;
+            
+            while (true) {
+                string userNumb = Console.ReadLine();
+                if (userNumb == "ok") {
+                    break;
+                }
+                int numbConvert = Convert.ToInt32(userNumb);
+                numbHolder += numbConvert;
+                numbConvert = 0;
+                Console.WriteLine("Enter your next number or type 'ok' to if that was your last number.");
+                }
+            Console.WriteLine("Great, you've finished typing your numbers, your final total is, \n {0}", numbHolder);
+        }
+    }
+}
